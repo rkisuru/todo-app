8a958b6da833c502085bca162ee8ec2324aa5d47
Isuru Aravinda <iaravinda33@gmail.com>
2024-04-26 18:29:47 +0530

    Added some boostrap classes, Added "OnPost" handler method to add new tasks to the list.

--- ToDo/Pages/Task/Add.cshtml.cs
+++ ToDo/Pages/Task/Add.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDo.Data;
+using ToDo.Models;
 
 namespace ToDo.Pages.Task
 {
+    [BindProperties]
     public class AddModel : PageModel
     {
+        private readonly AppDbContext _db;
+        public UserTask Task { get; set; }
+       
+        public AddModel(AppDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Task.AddAsync(Task);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
